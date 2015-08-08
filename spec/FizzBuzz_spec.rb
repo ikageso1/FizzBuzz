d3fba443196fd42046a1dec8a5b37cdd6e62f808
tenma <1210370051g@kindai.ac.jp>
2015-08-08 06:01:24 +0000

    first

---
+++ spec/FizzBuzz_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe FizzBuzz do
+  it 'has a version number' do
+    expect(FizzBuzz::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
